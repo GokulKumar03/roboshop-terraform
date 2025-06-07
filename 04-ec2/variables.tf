479917de00223fc5a7f1840485d96d9bd5be9f88
gokul <gokulkumar0307@gmail.com>
2025-06-07 16:42:39 +0530

    sg

---
+++ 04-ec2/variables.tf
@@ -0,0 +1,21 @@
+variable "common_tags" {
+    type = map
+    default = {
+        Project = "Roboshop"
+        Environment = "dev"
+        Terraform = "true"
+    }  
+}
+
+variable "sg_tags" {
+    type = map
+    default = {}  # it is optional
+}
+
+variable "project_name" {
+  default = "roboshop"
+}
+
+variable "environment" {
+  default = "dev"
+}
