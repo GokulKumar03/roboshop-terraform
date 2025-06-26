07dce2f370930d72d702c3a6a6735edb7e5ab7fc
gokul <gokulkumar0307@gmail.com>
2025-06-26 18:50:08 +0530

    main

--- 03-vpn/main.tf
+++ 03-vpn/main.tf
@@ -5,7 +5,7 @@ module "mongodb" {
   instance_type          = "t2.micro"
   vpc_security_group_ids = [data.aws_ssm_parameter.vpn_sg_id.value]
   subnet_id              = data.aws_subnet.selected.id
-  user_data = file("openvpn.sh")
+  #user_data = file("openvpn.sh")
   tags = merge(
     var.common_tags,
     {
