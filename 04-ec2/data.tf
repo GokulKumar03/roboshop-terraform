c48e9f2bea1319fcc78ba7132733ea278d1dfde2
gokul <gokulkumar0307@gmail.com>
2025-06-26 20:40:31 +0530

    main

--- 04-ec2/data.tf
+++ 04-ec2/data.tf
@@ -30,6 +30,15 @@ data "aws_subnet" "selected" {
 data "aws_ssm_parameter" "vpn_sg_id" {
   name = "/${var.project_name}/${var.environment}/vpn_sg_id"
 }
+
+data "aws_ssm_parameter" "mongodb_sg_id" {
+  name = "/${var.project_name}/${var.environment}/mongodb_sg_id"
+}
+
+data "aws_ssm_parameter" "database_subnet_ids" {
+  name = "/${var.project_name}/${var.environment}/database_subnet_ids"
+}
+
 # output "vpc_info" {
 #   value = data.aws_subnet.selected.id
 # }
